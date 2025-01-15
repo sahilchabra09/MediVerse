77836418478fc2af0a52e982f6c3b4113f9ecbed
Sahil Chabra <sahilchabra1509@gmail.com>
2025-01-15 18:38:25 +0530

    folder-structure-changed

--- Backend/rehab/rehab/api.py
+++
@@ -1,8 +0,0 @@
-from ninja import NinjaAPI
-from api.api import router as sensor_router  # Import the router
-
-# Create the main API instance
-api = NinjaAPI()
-
-# Include the router, assigning a path prefix (optional)
-api.add_router("/", sensor_router)
